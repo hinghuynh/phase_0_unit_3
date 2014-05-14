5b4daaa46071b10c8b77016e4f66d70e46878e4d
hinghuynh <hinghuynh@gmail.com>
2014-05-13 17:09:35 -0700

    Update our_solution.rb

--- week_7/7_gps3.1/our_solution.rb
+++ week_7/7_gps3.1/our_solution.rb
@@ -5,11 +5,11 @@
 # 2.
  
 # User Stories (As many as you want. Delete the statements you don't need)
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
+# As a user, I want to add item to grocery list
+# As a user, I want to delete item off grocery list
+# As a user, I want to see whats included list?
+# As a user, I want to see the entire list. show method
+# As a user, I want to add item with its quantity
  
 # Pseudocode
 # 
@@ -22,13 +22,50 @@
  
 # Your fabulous code goes here....
 
-
-
-
-
-
+class GroceryList 
+  def initialize()
+    @grocery_list = {}
+  end
+  
+  def add(*items) # items = 2 item array
+    items.each { |item| 
+      @grocery_list[item[0]] = item[1] 
+      p "#{item[1]} #{item[0].capitalize}(s) has been added to the list."
+    }
+  end
+  
+  def delete(item)
+    inc = @grocery_list.delete(item)
+    if (inc != nil)
+      p "#{item.capitalize} has been deleted from the list."
+    else
+      p "#{item.capitalize} is not found on the list."
+    end
+  end
+  
+  def included?(item)
+    inc = @grocery_list.has_key?(item)
+    if (inc != nil)
+      p "#{item.capitalize} is included in the list."
+    else
+      p "#{item.capitalize} is not included in the list."
+    end
+  end
+  
+  def show_list()
+    p "Grocery List:"
+    @grocery_list.each do |key, value|
+      p "#{key.capitalize} - #{value}"
+    end
+  end
+end
 
 # DRIVER CODE GOES HERE. 
  
- 
+list = GroceryList.new()
+list.add(['banana', 5], ['apple', 2])
+list.show_list
+list.delete('banana')
+list.included?('banana')
+list.show_list
  
